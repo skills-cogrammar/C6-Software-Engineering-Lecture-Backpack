6f9d0a9aef379ef01e02deeb746e64038df8e8ae
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2023-12-11 23:26:19 +0200

    Updates during Session
    The Strings.py file with added updates during session

---
+++ Week 1/1 - Theory and Practical/Monday/Strings-Session-Update.py
@@ -0,0 +1,42 @@
+# double_quote = "The quick brown fox jumps over the lazy dog"
+# double_quote = "Join said 'I love this session.'"
+# single_quote = 'A'
+
+# print(double_quote)
+
+# Assigning a string to a variable
+name = "Miles"
+surname = "Morales"
+
+# Assigning the result of two concatenated strings to a variable called 'full_name'
+full_name = name + surname
+print(full_name)
+
+full_name = name + " " + surname
+print(full_name)
+
+# BONUS ALERT!!! --> Using formatted-strings :)
+# full_name = f"My friend {name} has the {surname} surname."
+# full_name = f"{name} {surname}"
+
+# Fix convensions
+# Full Name='Riaan Deventer'
+# 1. full_name
+# 2. Single quotes should be double quotes "Riaan Deventer"
+# 3. Operator = should have space on both sides. This is called white space
+# Answer: full_name = "Riaan Deventer"
+
+# # Printing a user-friendly output to the terminal
+print("========= This is my name that is printed ========")
+full_name = "Riaan Deventer"
+print(f"My name is {full_name}")
+print(f"My friend's name is {name} {surname}")
+print('=' * 50)
+
+# my_string = "This is my mostest favoritous sentence"
+# my_integer = 5
+# my_float = 4.89
+# my_boolean = False
+# my_list = []
+# my_set = {}
+# my_tuple = ()
