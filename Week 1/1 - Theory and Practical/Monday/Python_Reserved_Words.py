70f35e87d6b0882614ae2cea06609ecc66ac0cab
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2023-12-11 17:03:16 +0200

    First Commit
    Added Week 1 Folders and Monday student material.

---
+++ Week 1/1 - Theory and Practical/Monday/Python_Reserved_Words.py
@@ -0,0 +1,11 @@
+'''
+False      class      finally    is         return
+None       continue   for        lambda     try
+True       def        from       nonlocal   while
+and        del        global     not        with
+as         elif       if         or         yield
+assert     else       import     pass
+break      except     in         raise
+'''
+
+# Also look out for Python built-in function names.
