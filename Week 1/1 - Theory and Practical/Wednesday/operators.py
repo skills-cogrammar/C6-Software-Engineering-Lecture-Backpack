dcc5d56cab2299c110ad44f3fee170130c0f759e
Serge-Mentor <79256268+Serge-Mentor@users.noreply.github.com>
2023-12-14 19:21:20 +0200

    Add files via upload

---
+++ Week 1/1 - Theory and Practical/Wednesday/operators.py
@@ -0,0 +1,54 @@
+# Addition
+a = 5
+b = 3
+result_addition = a + b
+print("Addition:", result_addition)
+
+# Subtraction
+result_subtraction = a - b
+print("Subtraction:", result_subtraction)
+
+# Multiplication
+result_multiplication = a * b
+print("Multiplication:", result_multiplication)
+
+# Division
+result_division = a / b
+print("Division:", result_division)
+
+# Exponentiation
+result_exponentiation = a ** b
+print("Exponentiation:", result_exponentiation)
+
+
+
+
+
+# Equality
+x = 10
+y = 20
+print("Equality:", x == y)
+
+# Inequality
+print("Inequality:", x != y)
+
+# Greater than
+print("Greater than:", x > y)
+
+# Less than
+print("Less than:", x < y)
+
+
+# AND
+condition1 = True
+condition2 = False
+result_and = condition1 and condition2
+print("Logical AND:", result_and)
+
+# OR
+result_or = condition1 or condition2
+print("Logical OR:", result_or)
+
+# NOT
+result_not = not condition1
+print("Logical NOT:", result_not)
