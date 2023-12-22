02f9203a92b27cc42ed4a86b8fecd09bcd7a17c4
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2023-12-22 09:41:04 +0200

    Add Learner Q to demo file

--- Week 2/1 - Theory and Practical/Thursday/for_or_while.py
+++ Week 2/1 - Theory and Practical/Thursday/for_or_while.py
@@ -72,6 +72,22 @@ while index < len(numbers):
 
 print(f"The sum of squares is: {sum_of_squares}")
 
+# -- Learner Question: Why i = 10 does not brake the loop?
+# Create stability and one-purpose assignment by using a new variable if you \
+# would like to use the temporary my_int variable after the loop execusion.
+for my_int in range(1, 4):      # This line will keep overwriting my_int = 10
+    print(my_int)
+    my_int = 10
+    print(my_int)
+    my_external_int = my_int
+
+print(my_external_int)
+
+# Slicing example. Try for range, ie. for my_int in range(1, 4, 2):
+my_string = "The sum of squares is"
+my_new_string = my_string[17:10:-1]  # parameters to print "squares" in reverse
+print(f">{my_new_string}<")
+
 # === Example 4: User Input Validation
 # -- while loop option --
 user_input = input("Enter a positive integer: ")
@@ -91,3 +107,5 @@ print(f"You entered: {user_input}")
 for my_int in range(1, 10000):
     if user_input.isdigit() or int(user_input) > 0:
         break
+    else:
+        user_input = input("Enter a positive integer: ")
